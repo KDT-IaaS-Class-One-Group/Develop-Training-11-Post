3a82cc308edce3d2a10e1ebb3ab34dc1d3b19470
samuel21695 <samuel21695@gmail.com>
2023-10-18 16:59:33 +0900

    signUpAsset 로그인 정보 저장하는 js 파일

---
+++ static/js/signUpAsset.js
@@ -0,0 +1,10 @@
+// 강사님께서 쓰신 signUpAsset 사용
+const signUpAsset= {
+  id: "",
+  password : "",
+  inputBoxColor : "#D9D9D9",
+  textColor : "B6B6B6",
+  pointColor: "FF6666"
+} 
+
+module.exports = signUpAsset; 

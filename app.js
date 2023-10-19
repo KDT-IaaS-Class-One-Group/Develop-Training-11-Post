197317978817d7777b4fee826559232af6e4827a
Yusmin97 <tmdamsi97@gmail.com>
2023-10-19 11:51:26 +0900

    method POST를 사용한 email페이지로 넘기겠금 작성

--- app.js
+++ app.js
@@ -30,37 +30,44 @@ const server = http.createServer((request, response) => {
       }
     });
     //* 기능 페이지 제작
-  } else if (request.method === 'POST' && parsedUrl.pathname === '/sign-up') {
-    // DB 저장용
-    let body = '';
-
-    request.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    // 콘솔 출력용
-    request.on('end', () => {
-      const parseBody = querystring.parse(body);
-      const { username, password, samePassword } = parseBody;
-
-      console.log('form 입력으로부터 받은 데이터 확인 -> ', parseBody);
-      console.log('form 입력으로부터 받은 데이터 확인 -> ', username);
-      console.log('form 입력으로부터 받은 데이터 확인 -> ', password);
-      console.log('form 입력으로부터 받은 데이터 확인 -> ', samePassword);
-
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('signUP success!');
-    });
-  } else if (request.method === 'GET' && request.url === '/e-mail') {
+  } else if (request.method === 'POST' && request.url === '/email') {
     fs.readFile('email.html', (err, data) => {
       if (err) {
         console.log('호출 에러');
       } else {
-        response.writeHead(200, contentT[0]);
-        response.end(data);
+        // DB 저장용
+        let body = '';
+
+        request.on('data', (chunk) => {
+          body += chunk.toString();
+        });
+
+        // 콘솔 출력용
+        request.on('end', () => {
+          const parseBody = querystring.parse(body);
+          const { username, password, samePassword } = parseBody;
+
+          console.log('form 입력으로부터 받은 데이터 확인 -> ', parseBody);
+          console.log('form 입력으로부터 받은 데이터 확인 -> ', username);
+          console.log('form 입력으로부터 받은 데이터 확인 -> ', password);
+          console.log('form 입력으로부터 받은 데이터 확인 -> ', samePassword);
+        });
       }
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(data);
     });
-  } else {
+  }
+  // else if (request.method === 'GET' && request.url === '/e-mail') {
+  //   fs.readFile('email.html', (err, data) => {
+  //     if (err) {
+  //       console.log('호출 에러');
+  //     } else {
+  //       response.writeHead(200, contentT[0]);
+  //       response.end(data);
+  //     }
+  //   });
+  // }
+  else {
     response.writeHead(404, { 'Content-Type': 'text/html; charset= utf-8' });
     response.end('<h1>요청 페이지를 찾을 수 없습니다</h1>');
   }

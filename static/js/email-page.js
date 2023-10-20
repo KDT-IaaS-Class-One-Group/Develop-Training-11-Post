9fdbfc5f7c17e9108229af45e08eeefb6f6bf41c
Yusmin97 <tmdamsi97@gmail.com>
2023-10-20 17:32:53 +0900

    이메일 페이지 js로 동적 생성 하여 모듈화

--- static/js/email-page.js
+++ static/js/email-page.js
@@ -1,14 +1,38 @@
 const emailPage = (body) => {
     return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1>${body}님! 접속을 환영함 ㅋ</h1>
-    </body>
+    <html lang="ko">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" href="./static/css/mailStyle.css" />
+      </head>
+      <body>
+        <div id="root">
+          <div>
+            <h1>
+              <pre>
+    ${body}님! 반갑습니다.
+    저에게 편지를 보내주세요!
+          </pre
+              >
+            </h1>
+          </div>
+          <form action="/send" method="POST">
+            <div>
+              <label for="title">title</label>
+              <input type="text" id="title" name="title" placeholder="제목" />
+            </div>
+            <div>
+              <label for="content">Text</label>
+              <input type="text" id="content" name="textSend" placeholder="내용" />
+            </div>
+            <div>
+              <input type="submit" name="submit" value="send" />
+            </div>
+          </form>
+        </div>
+      </body>
     </html>`
 } 
 
